5240849c428759e667f24966aada5ab43b8ddecc
Vlad <vlad.ios.develop@gmail.com>
2022-09-01 17:12:38 +0300

    Created UI

---
+++ ListFilms/ListFilms/ListFilmsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ListFilms
+//
+//  Created by Vlad Panchenko on 01.09.2022.
+//
+
+import UIKit
+
+class ListFilmsViewController: UIViewController {
+
+    @IBOutlet private var titleField: UITextField!
+    @IBOutlet private var yearField: UITextField!
+    @IBOutlet private var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction private func addAction(_ sender: Any) {
+
+    }
+
+}
+
